1c94aa1df3b86726cff7d78b00d0388858e316ad
DmitriiShavrin <102305084+DmitriiShavrin@users.noreply.github.com>
2022-04-23 18:12:33 +0600

    Delete Рабочий стол/game directory

--- Рабочий стол/game/src/components/game-genre/game-genre.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './game-genre.css';
-
-const GameGenre = ({ genre = '' }) => {
-    return <span className="game-genre">{ genre }</span>
-}
-
-export default GameGenre
